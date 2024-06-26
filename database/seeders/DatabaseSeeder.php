2b66371ce7123d4126440f6c1051045fc39946a8
mlemingcapoo <ducvu73@gmail.com>
2024-06-27 00:19:54 +0700

    Update: Complete DatabaseSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,19 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\About_us;
+use App\Models\Banners;
+use App\Models\Bill_details;
+use App\Models\Bills;
+use App\Models\Blogs;
+use App\Models\Cakes;
+use App\Models\Categories;
+use App\Models\Contact_us;
+use App\Models\Customer_requests;
+use App\Models\Promotions;
+use App\Models\User;
+use App\Models\Vouchers;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +25,140 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $jsonFilePath = "./data.json";
+        $jsonContent = file_get_contents($jsonFilePath);
+        $dataArray = json_decode($jsonContent, true);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($dataArray['contact_us'] as $row) {
+            Contact_us::create([
+                "phone_number" => $row['phone_number'],
+                "email" => $row['email'],
+                "address" => $row['address'],
+            ]);
+        }
+
+        foreach ($dataArray['about_us'] as $row) {
+            About_us::create([
+                "title" => $row['title'],
+                "content" => $row['content'],
+                "description" => $row['description'],
+                "image" => $row['image'],
+            ]);
+        }
+
+        foreach ($dataArray['vouchers'] as $row) {
+            Vouchers::create([
+                "code" => $row['code'],
+                "discount_type" => $row['discount_type'],
+                "discount_amount" => $row['discount_amount'],
+                "discount_percentage" => $row['discount_percentage'],
+                "description" => $row['description'],
+                "min_price" => $row['min_price'],
+                "quantity" => $row['quantity'],
+                "start_date" => $row['start_date'],
+                "end_date" => $row['end_date'],
+                "status" => $row['status'],
+            ]);
+        }
+
+        foreach ($dataArray['promotions'] as $row) {
+            Promotions::create([
+                "user_id" => $row['user_id'],
+                "cake_id" => $row['cake_id'],
+                "description" => $row['description'],
+                "start_time" => $row['start_time'],
+                "end_time" => $row['end_time'],
+                "discount_percentage" => $row['discount_percentage'],
+                "discount_amount" => $row['discount_amount'],
+                "discount_type" => $row['discount_type'],
+            ]);
+        }
+
+        foreach ($dataArray['customer_requests'] as $row) {
+            Customer_requests::create([
+                "name" => $row['name'],
+                "phone_number" => $row['phone_number'],
+                "email" => $row['email'],
+                "message" => $row['message'],
+                "status" => $row['status'],
+            ]);
+        }
+
+        foreach ($dataArray['users'] as $row) {
+            User::create([
+                "username" => $row['username'],
+                "password" => $row['password'],
+                "email" => $row['email'],
+                "address" => $row['address'],
+                "phone_number" => $row['phone_number'],
+                "role" => $row['role'],
+                "status" => $row['status'],
+            ]);
+        }
+
+        foreach ($dataArray['categories'] as $row) {
+            Categories::create([
+                "name" => $row['name'],
+                "description" => $row['description'],
+                "image" => $row['image'],
+            ]);
+        }
+
+        foreach ($dataArray['cakes'] as $row) {
+            Cakes::create([
+                "category_id" => $row['category_id'],
+                "name" => $row['name'],
+                "description" => $row['description'],
+                "real_price" => $row['real_price'],
+                "higher_price" => $row['higher_price'],
+                "image" => $row['image'],
+            ]);
+        }
+
+        foreach ($dataArray['blogs'] as $row) {
+            Blogs::create([
+                "user_id" => $row['user_id'],
+                "title" => $row['title'],
+                "subtitle" => $row['subtitle'],
+                "content" => $row['content'],
+                "images" => $row['images'],
+            ]);
+        }
+
+        foreach ($dataArray['banners'] as $row) {
+            Banners::create([
+                "title" => $row['title'],
+                "subtitle" => $row['subtitle'],
+                "image" => $row['image'],
+                "link" => $row['link'],
+                "start_date" => $row['start_date'],
+                "end_date" => $row['end_date'],
+                "status" => $row['status'],
+            ]);
+        }
+
+        foreach ($dataArray['bills'] as $row) {
+            Bills::create([
+                "order_date" => $row['order_date'],
+                "full_name" => $row['full_name'],
+                "address" => $row['address'],
+                "phone_number" => $row['phone_number'],
+                "email" => $row['email'],
+                "delivery_method" => $row['delivery_method'],
+                "checkout_method" => $row['checkout_method'],
+                "total_amount" => $row['total_amount'],
+                "status" => $row['status'],
+            ]);
+        }
+
+        foreach ($dataArray['bill_details'] as $row) {
+            Bill_details::create([
+                "cake_id" => $row['cake_id'],
+                "bill_id" => $row['bill_id'],
+                "quantity" => $row['quantity'],
+                "price" => $row['price'],
+                "total_price" => $row['total_price'],
+            ]);
+        }
     }
 }
