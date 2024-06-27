d87a5750917cd509dc4ee3bf5d10d5bdf3bd4eb1
mlemingcapoo <ducvu73@gmail.com>
2024-06-27 19:42:09 +0700

    Update: Fixing for rollback failure due to foreign key constraints

--- database/migrations/2024_06_26_000001_create_categories_table.php
+++ database/migrations/2024_06_26_000001_create_categories_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('products');
         Schema::dropIfExists('categories');
     }
 };
