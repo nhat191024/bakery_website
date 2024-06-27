f8f74f75b6ded98ee0218f00c3b478a5743f7ccd
Fumino The Fish <richberchannel01@gmail.com>
2024-06-27 20:51:04 +0700

    Update: rename file

--- database/migrations/2024_06_26_083328_create_about_us_table.php
+++ database/migrations/2024_06_26_083328_create_about_us_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->text('content');
             $table->text('image')->nullable();
             $table->timestamps();
         });
