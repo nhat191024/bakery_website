cadc30291c9b150a5d10066dfa25497f7ae8dd10
mlemingcapoo <ducvu73@gmail.com>
2024-06-27 19:46:26 +0700

    Create: Renamed model

---
+++ app/Models/Products.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $table = 'products';
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'real_price',
+        'higher_price',
+        'image',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsTo(Categories::class, 'category_id');
+    }
+
+    public function promotions()
+    {
+        return $this->hasMany(Promotions::class, 'product_id');
+    }
+
+    public function bill_details()
+    {
+        return $this->hasMany(Bill_details::class, 'product_id');
+    }
+}
