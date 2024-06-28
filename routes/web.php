56b850f395ed9940cb3808d7ff97af3ca416baad
LamTn <laoxonlonxao@gmail.com>
2024-06-28 22:46:48 +0700

    add template for admin screen

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,12 @@ Route::prefix('admin')->group(function () {
 });
 
 Route::get('/homePage', [HomePageController::class, 'index']);
+
+
+Route::prefix('admin')->group(function () {
+    Route::prefix('/category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.category.index');
+        Route::get('/add', [CategoryController::class, 'showAddCategory'])->name('admin.category.show_add');
+        Route::post('/add', [CategoryController::class, 'addCategory'])->name('admin.category.add');
+    });
+});
