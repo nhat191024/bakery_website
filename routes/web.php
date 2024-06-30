2cb850df0cea338becaaa2b246abd5d23d2759d6
mlemingcapoo <ducvu73@gmail.com>
2024-07-01 01:30:56 +0700

    Update: Adds login route and protect admin routes with Auth (middleware)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::prefix('admin')->group(function () {
+        // protected routes goes here
+    });
+});
+
+Route::get('/login', [App\Http\Controllers\LoginController::class, 'loginPage'])->name('main.login');
+Route::post('/login/auth', [App\Http\Controllers\LoginController::class, 'login'])->name('main.login.auth');
+Route::get('/logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('main.logout');
+
