93ab819ced6996636861bd4c91c6ffc55304d371
Fumino The Fish <richberchannel01@gmail.com>
2024-07-01 12:25:45 +0700

    Update: remove api key

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -193,8 +193,6 @@
     <script src="{{ URL::asset('js/jquery.animateNumber.min.js') }}"></script>
     <script src="{{ URL::asset('js/bootstrap-datepicker.js') }}"></script>
     <script src="{{ URL::asset('js/scrollax.min.js') }}"></script>
-    {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
-    {{-- <script src="js/google-map.js"></script> --}}
     <script src="{{ URL::asset('js/main.js') }}"></script>
 
 </body>
