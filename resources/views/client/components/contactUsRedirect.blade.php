26e1b2a41bfb77d4751e853b81edc8624bff4531
mlemingcapoo <ducvu73@gmail.com>
2024-07-03 19:21:22 +0700

    Create: Component for redirect to contact us page

---
+++ resources/views/client/components/contactUsRedirect.blade.php
@@ -0,0 +1,20 @@
+<section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
+    <div class="container py-4">
+        <div class="row d-flex justify-content-center py-5">
+            <div class="col-md-6">
+                <h2 style="font-size: 22px;" class="mb-0">Đăng kí nhận ưu đãi</h2>
+                <span>
+                    Nhận e-mail về những chương trình khuyến mại đặc biệt mới nhất!
+                </span>
+            </div>
+            <div class="col-md-6 align-items-center">
+                <div class="">
+                    <a href="#" class="btn form-control btn-primary" style="line-height: 200%"
+                        data-toggle="hover" data-bs-hover-animate="pulse">
+                        Đăng kí
+                    </a>
+                    </form>
+                </div>
+            </div>
+        </div>
+</section>
