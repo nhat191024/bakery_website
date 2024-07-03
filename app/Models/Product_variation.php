fd308a066afdbe1921cbea5c9a4a54980f89b1d6
mlemingcapoo <ducvu73@gmail.com>
2024-07-03 20:36:40 +0700

    Create: Model for Product variation

---
+++ app/Models/Product_variation.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product_variation extends Model
+{
+    protected $table = 'product_variations';
+
+    protected $fillable = [
+        'variation_id',
+        'product_id'
+    ];
+
+    public function variation()
+    {
+        return $this->belongsTo(Variation::class, 'variation_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Products::class, 'product_id');
+    }
+}
