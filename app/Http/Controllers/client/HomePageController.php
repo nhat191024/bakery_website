4f9000334caa016044876b07dc4308269661eccd
VuongVu <vuongvu10899@gmail.com>
2024-07-03 22:21:09 +0700

    Update Home Page lần 1 Co-authored-by: HuyLee <huylee47@users.noreply.github.com>

--- app/Http/Controllers/client/HomePageController.php
+++ app/Http/Controllers/client/HomePageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\client;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Message;
+use App\Models\Products;
 
 class HomePageController extends Controller
 {
+    // Bảng product
     public function index()
     {
-        return view('client.homePage');
+        $products = Products::orderBy('created_at', 'desc')->take(8)->get();
+        $messages = Message::orderBy('created_at', 'asc')->take(5)->get();
+        return view('client.homePage', compact('products', 'messages'));
     }
 }
