a9bda7baaa9b99190bcb7e5c6824b9a281f7b20b
VuongVu <vuongvu10899@gmail.com>
2024-07-03 23:30:56 +0700

    update home page lần 2

--- app/Http/Controllers/client/HomePageController.php
+++ app/Http/Controllers/client/HomePageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\client;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Banners;
 use App\Models\Message;
 use App\Models\Products;
 
@@ -14,6 +15,7 @@ class HomePageController extends Controller
     {
         $products = Products::orderBy('created_at', 'desc')->take(8)->get();
         $messages = Message::orderBy('created_at', 'asc')->take(5)->get();
-        return view('client.homePage', compact('products', 'messages'));
+        $images = Banners::all();
+        return view('client.homePage', compact('products', 'messages', 'images'));
     }
 }
