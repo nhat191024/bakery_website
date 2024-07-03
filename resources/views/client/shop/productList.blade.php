654949463e202674dd7c3e6a7f16eb88c7fe3f5d
mlemingcapoo <ducvu73@gmail.com>
2024-07-03 19:22:09 +0700

    Update: Implements contact us component

--- resources/views/client/shop/productList.blade.php
+++ resources/views/client/shop/productList.blade.php
@@ -56,22 +56,10 @@
             {{ $products->links('client.components.pagination') }}
         </div>
     </section>
-    <section class="ftco-section ftco-no-pt ftco-no-pb py-5 bg-light">
-        <div class="container py-4">
-            <div class="row d-flex justify-content-center py-5">
-                <div class="col-md-6">
-                    <h2 style="font-size: 22px;" class="mb-0">Đăng kí nhận ưu đãi</h2>
-                    <span>
-                        Nhận e-mail về những chương trình khuyến mại đặc biệt mới nhất!
-                    </span>
-                </div>
-                <div class="col-md-6 align-items-center">
-                    <div class="">
-                        <a href="#" class="btn form-control" style="line-height: 200%">Đăng kí</a>
-                        </form>
-                    </div>
-                </div>
-            </div>
-    </section>
+    <section>
+        @include('client.components.contactUsRedirect')
+    </section>    
     <script src="{{ URL::asset('js/aos.js.map') }}"></script>
 @endsection
+
+
