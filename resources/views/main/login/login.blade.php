92af0f787a9e8beab1fe0f323ab4f6a58f19ad6d
Fumino The Fish <richberchannel01@gmail.com>
2024-07-04 22:46:57 +0700

    Reformat

--- resources/views/main/login/login.blade.php
+++ resources/views/main/login/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,12 +8,13 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>SB Admin 2 - Login</title>
-    <link href="{{url('') . '/'}}vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{ url('') . '/' }}vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-    <link href="{{url('') . '/'}}css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{ url('') . '/' }}css/sb-admin-2.min.css" rel="stylesheet">
 </head>
+
 <body class="bg-gradient-success">
     <div class="container">
         <div class="row justify-content-center">
@@ -26,31 +28,31 @@
                                         <h1 class="h4 text-gray-900 mb-4">Đăng nhập để tiếp tục</h1>
                                     </div>
                                     <form class="user" method="POST" action="{{ route('main.login.auth') }}">
-                                      @csrf
+                                        @csrf
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="username" aria-describedby="emailHelp"
-                                                name="username"
+                                            <input type="text" class="form-control form-control-user" id="username"
+                                                aria-describedby="emailHelp" name="username"
                                                 placeholder="Username, email hoặc SĐT...">
-                                                @if ($errors->has('username'))
+                                            @if ($errors->has('username'))
                                                 <p class="text-danger small mt-1 text-center ">
                                                     <i>{{ $errors->first('username') }}</i>
                                                 </p>
-                                                @endif
+                                            @endif
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" name="password" placeholder="Mật khẩu">
-                                                @if ($errors->has('password'))
+                                            <input type="password" class="form-control form-control-user" id="password"
+                                                name="password" placeholder="Mật khẩu">
+                                            @if ($errors->has('password'))
                                                 <p class="text-danger small mt-1 text-center ">
                                                     <i>{{ $errors->first('password') }}</i>
                                                 </p>
-                                                @endif
+                                            @endif
                                         </div>
                                         <div class="form-group">
                                             @if (session('message'))
                                                 <div class="alert alert-danger">
-                                                    <i class="fa fa-exclamation-circle text-danger mr-2"></i> {{ session('message') }}
+                                                    <i class="fa fa-exclamation-circle text-danger mr-2"></i>
+                                                    {{ session('message') }}
                                                 </div>
                                             @endif
                                         </div>
@@ -66,9 +68,10 @@
             </div>
         </div>
     </div>
-    <script src="{{url('') . '/'}}vendor/jquery/jquery.min.js"></script>
-    <script src="{{url('') . '/'}}vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="{{url('') . '/'}}vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="{{url('') . '/'}}js/sb-admin-2.min.js"></script>
+    <script src="{{ url('') . '/' }}vendor/jquery/jquery.min.js"></script>
+    <script src="{{ url('') . '/' }}vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ url('') . '/' }}vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{ url('') . '/' }}js/sb-admin-2.min.js"></script>
 </body>
+
 </html>
