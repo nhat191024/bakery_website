4057bde89d7907d336ea91c08f2a95c9a2f4f31c
VuongVu <vuongvu10899@gmail.com>
2024-07-05 22:10:51 +0700

    update: ẩn paragraph

--- resources/views/client/blog/blogPage.blade.php
+++ resources/views/client/blog/blogPage.blade.php
@@ -86,12 +86,12 @@
                         </div>
                     </div>
 
-                    <div class="sidebar-box ftco-animate">
+                    {{-- <div class="sidebar-box ftco-animate">
                         <h3 class="heading">Paragraph</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus
                             voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur
                             similique, inventore eos fugit cupiditate numquam!</p>
-                    </div>
+                    </div> --}}
                 </div>
 
             </div>
