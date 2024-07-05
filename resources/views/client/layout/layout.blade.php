879e063d5439a6079f0414bde721a2fcc9d827d2
mlemingcapoo <ducvu73@gmail.com>
2024-07-05 19:22:51 +0700

    Update: Correct route links for the nav bar

--- resources/views/client/layout/layout.blade.php
+++ resources/views/client/layout/layout.blade.php
@@ -52,7 +52,7 @@
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Vegefoods</a>
+            <a class="navbar-brand" href="{{ route('client.home.index') }}">Vegefoods</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
                 aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="oi oi-menu"></span> Menu
@@ -60,12 +60,13 @@
 
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+                    <li class="nav-item active"><a href="{{ route('client.home.index') }}" class="nav-link">Home</a></li>
                     <li class="nav-item"><a href="{{ route('client.shop.productList')  }}" class="nav-link">Shop</a></li>
-                    <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-                    <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-                    <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
-                    <li class="nav-item cta cta-colored"><a href="cart.html" class="nav-link"><span
+                    <li class="nav-item"><a href="{{ route('client.about.index') }}" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="@if (Route::has('client.blog.index')){{ route('client.blog.index') }}
+                    @endif" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a href="{{ route('client.contact.index') }}" class="nav-link">Contact</a></li>
+                    <li class="nav-item cta cta-colored"><a href="{{ route('client.cart.index') }}" class="nav-link"><span
                                 class="icon-shopping_cart"></span>[0]</a></li>
 
                 </ul>
