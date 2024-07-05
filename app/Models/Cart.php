760ce57454125ca1fc9e2a12ec1f48f106025c48
mlemingcapoo <ducvu73@gmail.com>
2024-07-05 19:54:51 +0700

    Create: Init cart model

---
+++ app/Models/Cart.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+class Cart
+{
+
+    public static function add($product_id, $quantity = 1)
+    {
+
+    }
+
+    public static function remove($product_id)
+    {
+
+    }
+
+    public static function update($product_id, $quantity)
+    {
+
+    }
+
+    public static function get()
+    {
+        return [];
+    }
+
+    public static function totalAmount()
+    {
+        return 0;
+    }
+    
+    public static function totalQuantity()
+    {
+        return 0;
+    }
+}
