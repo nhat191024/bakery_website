d4cd7510188324a830c8bc2a5f300bc29ddf43c3
Fumino The Fish <richberchannel01@gmail.com>
2024-07-07 01:10:17 +0700

    Refactor: Improve cart functionality and fix bugs

--- resources/views/client/cart/cartProductsScript.blade.php
+++ resources/views/client/cart/cartProductsScript.blade.php
@@ -14,8 +14,6 @@
         });
     }
 
-
-
     function removeProduct(id) {
         $.ajax({
             url: "{{ route('client.cart.remove') }}",
@@ -30,9 +28,10 @@
                 // Check if the cart is empty after removing the product
                 if (data.cartCount === 0) {
                     subTotal = 0; // Reset subtotal if the cart is empty
-                }else{
-                    subTotal = subTotal - $('#product-' + id).find('.total').text().replace('đ', '').replace(
-                        ',', '').replace('.', '');
+                } else {
+                    subTotal = subTotal - $('#product-' + id).find('.total').text().replace('đ', '')
+                        .replace(
+                            ',', '').replace('.', '');
                 }
 
                 calculateTotal(data.discount);
@@ -40,91 +39,79 @@
         });
     }
 
+    // function updateProduct(id, quantity) {
+    //     $.ajax({
+    //         url: "{{ route('client.cart.update') }}",
+    //         method: "POST",
+    //         data: {
+    //             _token: "{{ csrf_token() }}",
+    //             'product_id': id,
+    //             'quantity': quantity
+    //         },
+    //         success: function(data) {
+    //             // Update the subtotal element in the UI immediately
+    //             $('#subTotal').text(new Intl.NumberFormat('de-DE').format(data.subTotal) + 'đ');
+    //             // $('#total-' + id).attr('value')
+    //             $('#total-' + id).text(new Intl.NumberFormat('de-DE').format(data.real_price) + 'đ');
+    //             console.log(data);
+    //             subTotal = data.subTotal;
+    //             calculateTotal(data.discount);
+    //         }
+    //     });
+    // }
 
+    // function calculateTotal(discount = 0) {
+    //     if (discount > 0) $('#removeDiscount').text('Xoá voucher');
+    //     else $('#removeDiscount').text('');
+    //     $('#totalPrice')
+    //         .text(subTotal - discount < 0 ? 0 : new Intl.NumberFormat('de-DE').format(subTotal - discount) + 'đ');
 
-    function updateProduct(id, quantity) {
-    $.ajax({
-        url: "{{ route('client.cart.update') }}",
-            method: "POST",
-            data: {
-                _token: "{{ csrf_token() }}",
-                'product_id': id,
-                'quantity': quantity
-            },
-        success: function (data) {
-            // Update the subtotal element in the UI immediately
-            $('#subTotal').text(new Intl.NumberFormat('de-DE').format(data.subTotal) + 'đ');
-            // $('#total-' + id).attr('value')
-            $('#total-'+id).text(
+    //     $('#subTotal')
+    //         .text(subTotal < 0 ? 0 : new Intl.NumberFormat('de-DE').format(subTotal) + 'đ');
 
-                new Intl.NumberFormat('de-DE').format(quantity * data.price)
+    //     $('#discountPrice')
+    //         .text((discount < 0 ? 0 : (new Intl.NumberFormat('de-DE').format(discount))) + 'đ');
+    // }
 
-                 + 'đ');
-                 console.log(data.quantity,' ', data.price);
-
-            subTotal = data.subTotal;
-            calculateTotal(data.discount);
-        }
-    });
-}
-
-    function calculateTotal(discount = 0) {
-
-        if (discount > 0) $('#removeDiscount').text('Xoá voucher');
-        else $('#removeDiscount').text('');
-        $('#totalPrice').text(subTotal - discount < 0 ? 0 : new Intl.NumberFormat('de-DE').format(subTotal - discount) +
-            'đ');
-
-        $('#subTotal').text(subTotal < 0 ? 0 : new Intl.NumberFormat('de-DE').format(subTotal) + 'đ');
-
-        $('#discountPrice').text((discount < 0 ? 0 : (new Intl.NumberFormat('de-DE').format(discount))) + 'đ');
-    }
     $(document).ready(function() {
         calculateTotal({{ $discount }});
 
-
         var quantitiy = 0;
 
+        // $('.quantity-right-plus').click(function(e) {
+        //     e.preventDefault();
+        //     const productId = $(this).data('id');
+        //     const quantityInput = $('#quantity-' + productId);
+        //     let quantity = parseInt(quantityInput.val()) + 1;
 
-        $('.quantity-right-plus').click(function(e) {
-            e.preventDefault();
-            const productId = $(this).data('id');
-            const quantityInput = $('#quantity-' + productId);
-            let quantity = parseInt(quantityInput.val()) + 1;
+        //     console.log(quantity);
+        //     quantity = Math.max(1, quantity);
+        //     quantityInput.val(quantity);
+        //     updateProduct(productId, quantity);
+        // });
 
+        // $('.quantity-left-minus').click(function(e) {
+        //     e.preventDefault();
+        //     const productId = $(this).data('id');
+        //     const quantityInput = $('#quantity-' + productId);
 
+        //     let quantity = parseInt(quantityInput.val()) - 1;
 
-            console.log();
-            quantity = Math.max(1, quantity);
-            quantityInput.val(quantity);
-            updateProduct(productId, quantity);
-        });
-
-
-        $('.quantity-left-minus').click(function(e) {
-            e.preventDefault();
-            const productId = $(this).data('id');
-            const quantityInput = $('#quantity-' + productId);
-
-            let quantity = parseInt(quantityInput.val()) - 1;
-
-            if (quantity >= 1) {
-                quantityInput.val(quantity);
-                updateProduct(productId, quantity);
-            } else {
-                if (confirm('Xoá sản phẩm này khỏi giỏ hàng?')) {
-                    removeProduct(productId);
-                }
-            }
-        });
-
+        //     if (quantity >= 1) {
+        //         quantityInput.val(quantity);
+        //         updateProduct(productId, quantity);
+        //     } else {
+        //         if (confirm('Xoá sản phẩm này khỏi giỏ hàng?')) {
+        //             removeProduct(productId);
+        //         }
+        //     }
+        // });
 
         $('#addVoucher').click(function(e) {
             e.preventDefault();
             var voucher_code = $('#voucherCode').val();
 
             $.ajax({
-
                 url: "{{ route('client.cart.applyVoucher') }}",
                 method: "POST",
                 data: {
@@ -169,14 +156,11 @@
                             $('#discountPrice').text(new Intl.NumberFormat('de-DE').format(
                                 res) + 'đ');
                             calculateTotal(res);
-
                             break;
                     }
-
                 },
                 error: function(e) {}
             });
         });
     });
 </script>
-
