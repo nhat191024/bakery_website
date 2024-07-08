026bea6767daf2db2ac0690f8ee966eb83cb216b
Fumino The Fish <richberchannel01@gmail.com>
2024-07-08 19:19:42 +0700

    Refactor: Update CheckoutService to create a bill and bill details upon confirming an order

--- app/Service/client/CheckoutService.php
+++ app/Service/client/CheckoutService.php
@@ -16,17 +16,48 @@ class CheckoutService
     public function index()
     {
         return view('client.checkout.checkout')
-        ->with([
-            'cart' => Cart::get(),
-            'subTotal' => Cart::getSubtotal(),
-            'discount' => Cart::getDiscountAmount(),
-            'total' => Cart::getTotal(),
-        ]);
+            ->with([
+                'cart' => Cart::get(),
+                'subTotal' => Cart::getSubtotal(),
+                'discount' => Cart::getDiscountAmount(),
+                'total' => Cart::getTotal(),
+            ]);
     }
 
     public function confirmOrder($request)
     {
-        dd($request->all());
-        return 'received: '.$request->all();
+        $bill = Bills::create([
+            'order_date' => now(),
+            'full_name' => $request->fullName,
+            'address' => $request->address . ', ' . $request->ward . ', ' . $request->district . ', ' . "Hà Nội",
+            'phone_number' => $request->phone,
+            'email' => $request->email,
+            'voucher_code' => Cart::getCouponCode() ? Cart::getCouponCode() : null,
+            'delivery_method' => $request->delivery,
+            'payment_method' => $request->payment,
+            'total_amount' => Cart::getTotal() ? Cart::getTotal() : 0,
+            'status' => 1,
+        ]);
+
+        $cart = Cart::get();
+        foreach ($cart as $item) {
+            foreach ($item['product']['product_variations'] as $variation) {
+                if ($item['variation_id'] == $variation['variation_id']) {
+                    $billDetail = Bill_details::create([
+                        'bill_id' => $bill->id,
+                        'product_id' => $item['product']['id'],
+                        'variation_id' => $item['variation_id'],
+                        'quantity' => $item['quantity'],
+                        'price' => $variation['price'],
+                    ]);
+                }
+            }
+        }
+        if ($billDetail) {
+            Cart::clear();
+            return response()->json([
+                'message' => 'success'
+            ], 200);
+        }
     }
 }
