5acde0de85848a451cc6710c994b80289850158c
Fumino The Fish <richberchannel01@gmail.com>
2024-07-08 19:24:10 +0700

    chore: Remove unnecessary header section from client layout template

--- resources/views/client/layout/layout.blade.php
+++ resources/views/client/layout/layout.blade.php
@@ -27,29 +27,6 @@
 </head>
 
 <body class="goto-here">
-    <div class="py-1 bg-primary">
-        <div class="container">
-            <div class="row no-gutters d-flex align-items-start align-items-center px-md-0">
-                <div class="col-lg-12 d-block">
-                    <div class="row d-flex">
-                        <div class="col-md pr-4 d-flex topper align-items-center">
-                            <div class="icon mr-2 d-flex justify-content-center align-items-center"><span
-                                    class="icon-phone2"></span></div>
-                            <span class="text">+84 24 6291 8575</span>
-                        </div>
-                        <div class="col-md pr-4 d-flex topper align-items-center">
-                            <div class="icon mr-2 d-flex justify-content-center align-items-center"><span
-                                    class="icon-paper-plane"></span></div>
-                            <span class="text">odouceurs@gmail.com</span>
-                        </div>
-                        <div class="col-md-5 pr-4 d-flex topper align-items-center text-lg-right">
-                            <span class="text">Giao hàng nhanh chóng &amp; Miễn phí nội thành</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container">
             <a class="navbar-brand" href="index.html">Odouceurs</a>
