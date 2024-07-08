5962164792459e1ab7720ce905a1a17071aad019
Fumino The Fish <richberchannel01@gmail.com>
2024-07-08 20:29:10 +0700

    Update: categoriesR query in HomePageController

--- app/Http/Controllers/client/HomePageController.php
+++ app/Http/Controllers/client/HomePageController.php
@@ -18,7 +18,7 @@ class HomePageController extends Controller
     {
         $products = Products::orderBy('created_at', 'desc')->take(8)->get();
         $categoriesL = Categories::orderBy('id','asc')->take(2)->get();
-        $categoriesR = Categories::orderBy('id','desc')->take(1)->get();
+        $categoriesR = Categories::orderBy('id','desc')->take(2)->get();
         $messages = Message::orderBy('created_at', 'asc')->take(5)->get();
         $images = Banners::all();
         $promotions = Promotions::orderBy('created_at', 'asc')->take(1)->get();
