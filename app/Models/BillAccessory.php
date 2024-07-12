f07262702e519424d3cf21a7ad240de46a552f8e
mlemingcapoo <ducvu73@gmail.com>
2024-07-12 13:05:52 +0700

    Update: Model BillAccessory

--- app/Models/BillAccessory.php
+++ app/Models/BillAccessory.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class BillAccessory extends Model
 {
-    use HasFactory;
+    protected $table = 'bill_accessories';
+
+    protected $fillable = [
+        'bill_id',
+        'accessory_id',
+    ];
+
+    public function accessories()
+    {
+        return $this->belongsTo(Accessory::class, 'accessory_id');
+    }
+
+    public function bills()
+    {
+        return $this->belongsTo(Bills::class, 'bill_id');
+    }
 }
