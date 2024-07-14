eef39a9869d0c2ee528f0f874b670c7bcf2876f6
mlemingcapoo <ducvu73@gmail.com>
2024-07-15 04:45:45 +0700

    Update: Fix status and add a function to calculate time passed by

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -1,23 +1,49 @@
 <?php
+
 namespace App\Helper;
 
-class Helper {
+use Carbon\Carbon;
+
+class Helper
+{
     public const DELIVERY_SHIP = 1;
-    public const DELIVERY_COUNTER = 0;
-    public const PAYMENT_CASH = 0;
-    public const PAYMENT_TRANSFER = 0;
+    public const DELIVERY_COUNTER = 2;
+    public const PAYMENT_CASH = 1;
+    public const PAYMENT_TRANSFER = 2;
     public const BILL_UNPAID = 0;
     public const BILL_PAID = 1;
     public const DELIVERY_METHOD = [
-        '0' => 'Lấy tại quầy',
-        '1' => 'Ship tận nơi'
+        '1' => 'Ship tận nơi',
+        '2' => 'Lấy tại quầy'
     ];
     public const PAYMENT_METHOD = [
-        '0' => 'Tiền mặt',
-        '1' => 'Chuyển khoản'
+        '1' => 'Tiền mặt',
+        '2' => 'Chuyển khoản'
     ];
     public const BILL_STATUS = [
         '0' => 'Chưa thanh toán',
         '1' => 'Đã thanh toán'
     ];
+
+    public static function getTimePassedBy($created_at)
+    {
+        $now = Carbon::now();
+        $createdAt = Carbon::parse($created_at);
+        $diff = $createdAt->diffForHumans($now);
+        $diff = str_replace(' seconds', ' giây', $diff);
+        $diff = str_replace(' second', ' giây', $diff);
+        $diff = str_replace(' minutes', ' phút', $diff);
+        $diff = str_replace(' minute', ' phút', $diff);
+        $diff = str_replace(' hours', ' giờ', $diff);
+        $diff = str_replace(' hour', ' giờ', $diff);
+        $diff = str_replace(' months', ' tháng', $diff);
+        $diff = str_replace(' month', ' tháng', $diff);
+        $diff = str_replace(' years', ' năm', $diff);
+        $diff = str_replace(' year', ' năm', $diff);
+        $diff = str_replace(' ago', ' trước', $diff);
+        $diff = str_replace(' before', ' trước', $diff);
+        $diff = str_replace(' later', ' tới', $diff);
+        $diff = str_replace(' after', ' tới', $diff);
+        return $diff;
+    }
 }
