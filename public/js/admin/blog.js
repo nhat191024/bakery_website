cf23fe637a7c18f8e81ee9a3ef638f292da2eae1
nhat191024 <richberchannel01@gmail.com>
2024-07-17 13:37:46 +0700

    feat: Add rich text editor for blog post creation and editing

---
+++ public/js/admin/blog.js
@@ -0,0 +1,66 @@
+const options = {
+    placeholder: 'Nội dung blog...!',
+    theme: 'snow',
+    modules: {
+        toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote'],
+            ['link', 'image', 'video', 'formula'],
+
+            // [{ 'header': 1 }, { 'header': 2 }],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'list': 'check' }],
+            // [{ 'script': 'sub' }, { 'script': 'super' }],
+            [{ 'indent': '-1' }, { 'indent': '+1' }],
+            [{ 'direction': 'rtl' }],
+
+            [{ 'size': ['small', false, 'large', 'huge'] }],
+            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+            [{ 'color': [] }, { 'background': [] }],
+            [{ 'font': [] }],
+            [{ 'align': [] }],
+
+            ['clean']
+        ]
+    }
+};
+
+var editorContainer = document.getElementById('editor');
+if (editorContainer) {
+    var quill = new Quill('#editor', options);
+    quill.root.innerHTML = blogContent;
+}
+
+const title = $('#title');
+const subtitle = $('#subtitle');
+// const content = $('#content');
+const id = $('#id');
+
+$(document).ready(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        // content.val(quill.root.innerHTML);
+    });
+})
+
+function edit() {
+    $.ajax({
+        url: "/admin/blog/edit",
+        method: "POST",
+        data: {
+            _token: csrfToken,
+            'title': title.val(),
+            'subtitle': subtitle.val(),
+            'content': quill.root.innerHTML,
+            'id': id.val()
+        },
+        success: function (data) {
+            window.location.reload();
+        }
+    });
+}
+
+
+
+
+
