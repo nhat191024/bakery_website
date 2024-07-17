94b842f4be3e48f9e6709566877683f364c0ab2c
nhat191024 <richberchannel01@gmail.com>
2024-07-17 14:39:21 +0700

    Refactor: form submission logic in blog.js

--- public/js/admin/blog.js
+++ public/js/admin/blog.js
@@ -39,7 +39,6 @@ const id = $('#id');
 $(document).ready(function () {
     $('form').on('submit', function (e) {
         e.preventDefault();
-        // content.val(quill.root.innerHTML);
     });
 })
 
