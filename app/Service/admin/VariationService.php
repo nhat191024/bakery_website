3d88ea9c0597b70a4527d7185c1352afe90f1498
mlemingcapoo <ducvu73@gmail.com>
2024-07-19 13:53:17 +0700

    feat: Add soft delete functionality for variations

--- app/Service/admin/VariationService.php
+++ app/Service/admin/VariationService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Service\admin;
 
 use App\Models\Variation;
@@ -7,7 +8,7 @@ class VariationService
 {
     public function getAll()
     {
-        $variations = Variation::all();
+        $variations = Variation::withTrashed()->get();
         return $variations;
     }
 
@@ -19,6 +20,25 @@ class VariationService
 
     public function update($id, $name)
     {
-       return VariationService::getById($id)->update(['name' => $name]);
+        return VariationService::getById($id)->update(['name' => $name]);
+    }
+    public function delete($id)
+    {
+        return VariationService::getById($id)->delete();
+    }
+    public function restore($id)
+    {
+        try {
+            Variation::withTrashed()->where('id', $id)->restore();
+            return true;
+        } catch (\Throwable $th) {
+            return false;
+        }
+    }
+    public function add($name)
+    {
+        return Variation::create([
+            'name' => $name,
+        ])->id;
     }
 }
