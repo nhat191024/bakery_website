98da988a5b897d9f6d32be5b31f222106b1ab5c5
mlemingcapoo <ducvu73@gmail.com>
2024-07-19 13:53:54 +0700

    Fix: Add missing button type attribute

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -240,7 +240,7 @@
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <form class="form-inline">
-                        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <button type="button" id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                             <i class="fa fa-bars"></i>
                         </button>
                     </form>
