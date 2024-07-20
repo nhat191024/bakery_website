679054e33224937ca1b1ff5d10f4ea90d22d2382
nhat191024 <richberchannel01@gmail.com>
2024-07-21 03:11:00 +0700

    Update: routes/web.php to add language switcher functionality

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\client\HomePageController;
 use App\Http\Controllers\client\ProductDetailController;
 use App\Http\Controllers\client\ProductListController;
 use App\Http\Controllers\client\BlogController;
+use App\Http\Controllers\client\LanguageController;
 
 use App\Models\Cart;
 
@@ -35,37 +36,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomePageController::class, 'index'])->name('client.homepage.index');
-Route::get('/about', [AboutController::class, 'index'])->name('client.about.index');
-Route::prefix('contact')->group(function () {
-    Route::get('/', [ContactController::class, 'index'])->name('client.contact.index');
-    Route::post('/', [ContactController::class, 'store'])->name('client.contact.store');
+Route::middleware(['localization'])->group(function () {
+    Route::get('/', [HomePageController::class, 'index'])->name('client.homepage.index');
+
+    Route::get('/about', [AboutController::class, 'index'])->name('client.about.index');
+    Route::prefix('contact')->group(function () {
+        Route::get('/', [ContactController::class, 'index'])->name('client.contact.index');
+        Route::post('/', [ContactController::class, 'store'])->name('client.contact.store');
+    });
+
+    Route::prefix('blog')->group(function () {
+        Route::get('/', [BlogController::class, 'index'])->name('client.blog.index');
+        Route::get('/{id}', [BlogController::class, 'show'])->name('client.blog.show');
+    });
+
+    Route::prefix('shop')->group(function () {
+        Route::get('/{categoryId?}', [ProductListController::class, 'index'])->name('client.shop.productList');
+        Route::get('/product/{productId}', [ProductDetailController::class, 'index'])->name('client.shop.productDetail');
+    });
+
+    Route::prefix('cart')->group(function () {
+        Route::get('/', [CartController::class, 'index'])->name('client.cart.index');
+        Route::post('/add', [CartController::class, 'addToCart'])->name('client.cart.add');
+        Route::post('/update', [CartController::class, 'updateCart'])->name('client.cart.update');
+        Route::post('/remove', [CartController::class, 'removeFromCart'])->name('client.cart.remove');
+        Route::post('/applyVoucher', [CartController::class, 'applyVoucher'])->name('client.cart.applyVoucher');
+        Route::post('/removeVoucher', [CartController::class, 'removeVoucher'])->name('client.cart.removeVoucher');
+        Route::get('/getCount', [Cart::class, 'getCartCount'])->name('cart.getCartCount');
+    });
+
+    Route::prefix('checkout')->group(function () {
+        Route::get('/', [CheckoutController::class, 'index'])->name('client.checkout.index');
+        Route::post('/confirm', [CheckoutController::class, 'confirmOrder'])->name('client.checkout.store');
+    });
 });
 
-Route::prefix('blog')->group(function () {
-    Route::get('/', [BlogController::class, 'index'])->name('client.blog.index');
-    Route::get('/{id}', [BlogController::class, 'show'])->name('client.blog.show');
-});
 
-Route::prefix('shop')->group(function () {
-    Route::get('/{categoryId?}', [ProductListController::class, 'index'])->name('client.shop.productList');
-    Route::get('/product/{productId}', [ProductDetailController::class, 'index'])->name('client.shop.productDetail');
-});
-
-Route::prefix('cart')->group(function () {
-    Route::get('/', [CartController::class, 'index'])->name('client.cart.index');
-    Route::post('/add', [CartController::class, 'addToCart'])->name('client.cart.add');
-    Route::post('/update', [CartController::class, 'updateCart'])->name('client.cart.update');
-    Route::post('/remove', [CartController::class, 'removeFromCart'])->name('client.cart.remove');
-    Route::post('/applyVoucher', [CartController::class, 'applyVoucher'])->name('client.cart.applyVoucher');
-    Route::post('/removeVoucher', [CartController::class, 'removeVoucher'])->name('client.cart.removeVoucher');
-    Route::get('/getCount', [Cart::class, 'getCartCount'])->name('cart.getCartCount');
-});
-
-Route::prefix('checkout')->group(function () {
-    Route::get('/', [CheckoutController::class, 'index'])->name('client.checkout.index');
-    Route::post('/confirm', [CheckoutController::class, 'confirmOrder'])->name('client.checkout.store');
-});
+Route::get('/language', [LanguageController::class, 'changeLanguage'])->name('change-language');
 
 Route::get('/login', [App\Http\Controllers\LoginController::class, 'loginPage'])->name('main.login');
 Route::post('/login/auth', [App\Http\Controllers\LoginController::class, 'login'])->name('main.login.auth');
