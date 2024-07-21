cfca55d2b00f3c0c61450011c165fa787b6228bc
HuyLee <huyw300@gmail.com>
2024-07-21 21:22:34 +0700

    Update : Scaleup logo & font size navbar/Change logo Footer/add phone contact footer/ Change background color Footer & navbar

--- lang/vi/contact.php
+++ lang/vi/contact.php
@@ -6,7 +6,8 @@ return [
     "address" => "Địa chỉ",
     "address1" => "Cơ sở 1 : 72 Đ. Xuân Diệu, Tứ Liên, Tây Hồ, Hà Nội",
     "address2" => "Cơ sở 2 : 56d P. Trần Nhân Tông, Nguyễn Du, Hai Bà Trưng, Hà Nội",
-    "phone" => "Số điện thoại",
+    "phone1" => "Số điện thoại",
+    "phone2" => "Số điện thoại",
     "social" => "Mạng xã hội",
     "message" => [
         "name" => "Tên của bạn",
