a846157eb84ee872b6e7815a742a375b2d119f59
mlemingcapoo <ducvu73@gmail.com>
2024-07-21 17:49:08 +0700

    feat: Add English translations to product names and descriptions
    Adds English translations for product names and descriptions to support multilingual content display. This allows the website to cater to a wider audience and potentially increase reach.

--- database/migrations/2024_06_26_000002_create_products_table.php
+++ database/migrations/2024_06_26_000002_create_products_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('category_id');
             $table->string('name');
+            $table->string('name_en');
             $table->text('description')->nullable();
+            $table->text('description_en')->nullable();
             $table->text('image')->nullable();
             $table->timestamps();
             $table->foreign('category_id')->references('id')->on('categories');
