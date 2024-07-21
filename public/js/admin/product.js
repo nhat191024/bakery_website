3e4bdd0bcea2a38bf67ec6791bb67865a5b71899
mlemingcapoo <ducvu73@gmail.com>
2024-07-21 17:50:57 +0700

    feat: Add English product name and description
    Adds English versions of product name and description to the product creation form, allowing for multilingual support.  This enhances the user experience by accommodating a broader audience.

--- public/js/admin/product.js
+++ public/js/admin/product.js
@@ -5,12 +5,14 @@ $('form').on('submit', function (e) {
     formData.append('_token', token);
     formData.append('category_id', $('#categorySelect').val());
     formData.append('product_name', $('#productName').val());
+    formData.append('product_name_en', $('#productName_en').val());
 
     selectedSizeList.forEach(function(item, index) {
         formData.append(`product_price[${index}]`, JSON.stringify(item));
     });
 
     formData.append('product_description', $('#productDescription').val());
+    formData.append('product_description_en', $('#productDescription_en').val());
     formData.append('product_image', $('#customFile')[0].files[0]);
 
     $.ajax({
