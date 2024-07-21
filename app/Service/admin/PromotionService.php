c2fd9f5de6e45b4de848232c4c597a4ab8a8468b
mlemingcapoo <ducvu73@gmail.com>
2024-07-21 17:07:39 +0700

    feat: Implement fake price calculation for promotions
    Adds logic to calculate a fake price for promotions based on the product's stock price. This fake price will be used to display a promotional price for the product.
    
    The fake price is calculated by adding 30% of the stock price to the stock price.

--- app/Service/admin/PromotionService.php
+++ app/Service/admin/PromotionService.php
@@ -17,9 +17,17 @@ class PromotionService
     {
         $promotion = PromotionService::get();
         $promotion->product_id = $request->product_id;
-        $promotion->description = $request->description;
+        $promotion->fake_price = $this->getFakePriceByStockPrice($request->product_id);
         $promotion->start_time = $request->start_time;
         $promotion->end_time = $request->end_time;
         return $promotion->save();
+    }
+
+    public function getFakePriceByStockPrice($product_id)
+    {
+        $service = new ProductService();
+        $price = $service->getDetailById($product_id)->price;
+        $fake_price = $price + ($price * 30 / 100);
+        return $fake_price;
     }
 }
