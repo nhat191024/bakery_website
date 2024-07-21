de2df2b5158b949cde10875f3c7195ac0eee25cd
mlemingcapoo <ducvu73@gmail.com>
2024-07-21 15:41:39 +0700

    feat: improve bill detail page UI
    Improved the UI of the bill detail page by adding a section for accessories and restructuring the information display for better clarity and organization.

--- resources/views/admin/bill/bill_detail.blade.php
+++ resources/views/admin/bill/bill_detail.blade.php
@@ -13,7 +13,8 @@
 
             <!-- Page Heading -->
             <div class="d-flex justify-content-between">
-                <h1 class="h3 mb-2 text-gray-800">Chi tiết hóa đơn #{{ $billInfo->id }} - Nhận {{ $helper::getTimePassedBy($billInfo->created_at) }}</h1>
+                <h1 class="h3 mb-2 text-gray-800">Chi tiết hóa đơn #{{ $billInfo->id }} - Nhận
+                    {{ $helper::getTimePassedBy($billInfo->created_at) }}</h1>
                 <a href="{{ route('admin.bill.index') }}" class="btn btn-primary mb-1">Back</a>
             </div>
             <!-- DataTales Example -->
@@ -30,52 +31,69 @@
                                 <strong>{{ $message }}</strong>
                             </div>
                         @endif
-                            <div class="form-group">
-                                <p><strong>Tên người đặt: </strong>{{ $billInfo->full_name }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Địa chỉ: </strong>{{ $billInfo->address }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Số điện thoại: </strong>{{ $billInfo->phone_number }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Email: </strong>{{ $billInfo->email }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Voucher code: </strong>{{ $billInfo->voucher_code }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Phương thức giao hàng:
-                                    </strong>{{ Helper::DELIVERY_METHOD[$billInfo->delivery_method] }}</p>
-                            </div>
-                            <div class="form-group">
-                                <p><strong>Phương thức thanh toán:
-                                    </strong>{{ Helper::PAYMENT_METHOD[$billInfo->payment_method] }}</p>
-                            </div>
-                            <div class="form-group">
-                                <h5 class="text-gray-900"><strong>Tổng tiền: </strong>{{ number_format($billInfo->total_amount) }}đ</h5>
-                            </div>
-                            <p class="{{ $billInfo->status == 0 ? 'text-danger' : ($billInfo->status == 2 ? 'text-warning' : 'text-success') }}"><strong>Trạng thái thanh toán: </strong>
-                                {{ Helper::BILL_STATUS[$billInfo['status']] }}</p>
+                        <div class="form-group">
+                            <p><strong>Tên người đặt: </strong>{{ $billInfo->full_name }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Địa chỉ: </strong>{{ $billInfo->address }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Số điện thoại: </strong>{{ $billInfo->phone_number }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Email: </strong>{{ $billInfo->email }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Voucher code: </strong>{{ $billInfo->voucher_code }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Phương thức giao hàng:
+                                </strong>{{ Helper::DELIVERY_METHOD[$billInfo->delivery_method] }}</p>
+                        </div>
+                        <div class="form-group">
+                            <p><strong>Phương thức thanh toán:
+                                </strong>{{ Helper::PAYMENT_METHOD[$billInfo->payment_method] }}</p>
+                        </div>
+                        <div class="form-group">
+                            <h5 class="text-gray-900"><strong>Tổng tiền:
+                                </strong>{{ number_format($billInfo->total_amount) }}đ</h5>
+                        </div>
+                        <p
+                            class="{{ $billInfo->status == 0 ? 'text-danger' : ($billInfo->status == 2 ? 'text-warning' : 'text-success') }}">
+                            <strong>Trạng thái thanh toán: </strong>
+                            {{ Helper::BILL_STATUS[$billInfo['status']] }}</p>
 
-                            <input type="hidden" name="bill_id" value="{{ $billInfo['id'] }}">
-                            <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo['id'], 'bill_status' => Helper::BILL_UNPAID]) }}" class="btn btn-warning mb-4"
-                                onclick="return confirmAction('Đánh dấu là CHƯA THANH TOÁN?', this.href)">
-                                Đánh dấu là chưa thanh toán
-                            </a>
-                            <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo->id, 'bill_status' => Helper::BILL_PAID]) }}" class="btn btn-success mb-4"
-                                onclick="return confirmAction('Đánh dấu là ĐÃ THANH TOÁN?', this.href)">
-                                Đánh dấu là đã thanh toán
-                            </a>
-                            <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo->id, 'bill_status' => Helper::BILL_CANCEL]) }}" class="btn btn-danger mb-4"
-                                onclick="return confirmAction('Xác nhận HUỶ ĐƠN?', this.href)">
-                                Huỷ đơn
-                            </a>
-                            <br>
-                            <a href="{{ route('admin.bill.index') }}" class="btn btn-primary mb-1">Quay về</a>
+                        <input type="hidden" name="bill_id" value="{{ $billInfo['id'] }}">
+                        <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo['id'], 'bill_status' => Helper::BILL_UNPAID]) }}"
+                            class="btn btn-warning mb-4"
+                            onclick="return confirmAction('Đánh dấu là CHƯA THANH TOÁN?', this.href)">
+                            Đánh dấu là chưa thanh toán
+                        </a>
+                        <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo->id, 'bill_status' => Helper::BILL_PAID]) }}"
+                            class="btn btn-success mb-4"
+                            onclick="return confirmAction('Đánh dấu là ĐÃ THANH TOÁN?', this.href)">
+                            Đánh dấu là đã thanh toán
+                        </a>
+                        <a href="{{ route('admin.bill.updateStatus', ['bill_id' => $billInfo->id, 'bill_status' => Helper::BILL_CANCEL]) }}"
+                            class="btn btn-danger mb-4" onclick="return confirmAction('Xác nhận HUỶ ĐƠN?', this.href)">
+                            Huỷ đơn
+                        </a>
+                        <br>
+                        <a href="{{ route('admin.bill.index') }}" class="btn btn-primary mb-1">Quay về</a>
                         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <hr>
                             <h3>Danh sách sản phẩm có trong đơn hàng này</h3>
+                            @if ($billInfo->accessory && $billInfo->accessory->name && $billInfo->accessory->price)
+                                <h4 class="font-weight-bold text-info">
+                                    Phụ kiện: {{ $billInfo->accessory->name }} -
+                                    {{ number_format($billInfo->accessory->price) }}đ
+                                </h4>
+                                <small class="text-info mb-3">({{ $billInfo->accessory->description ?? '' }})</small>
+                            @else
+                                <h4 class="font-weight-bold text-info">Khách không chọn phụ kiện</h4>
+                            @endif
+
+                            <hr>
                             <thead>
                                 <tr>
                                     <th>STT</th>
@@ -100,8 +118,10 @@
                                 @foreach ($billInfo->bill_details as $key => $item)
                                     <tr>
                                         <td>{{ ++$key }}</td>
-                                        <td>{{ Helper::getWithTrashedProductById($item->product_id)?->name ?? 'Sản phẩm đã bị xoá' }}</td>
-                                        <td>{{ Helper::getWithTrashedVariationById($item->variation_id)?->name ?? 'Size đã bị xoá' }}</td>
+                                        <td>{{ Helper::getWithTrashedProductById($item->product_id)?->name ?? 'Sản phẩm đã bị xoá' }}
+                                        </td>
+                                        <td>{{ Helper::getWithTrashedVariationById($item->variation_id)?->name ?? 'Size đã bị xoá' }}
+                                        </td>
 
                                         <td>{{ $item->quantity }}</td>
                                         <td>{{ number_format($item->price, 0, ',', '.') }}đ</td>
@@ -111,7 +131,8 @@
                             </tbody>
                         </table>
                         <div class="form-group text-center">
-                            <h3 class="mt-3 text-gray-900"><strong>Tổng tiền: </strong>{{ number_format($billInfo->total_amount) }}đ</h3>
+                            <h3 class="mt-3 text-gray-900"><strong>Tổng tiền:
+                                </strong>{{ number_format($billInfo->total_amount) }}đ</h3>
                         </div>
                     </div>
                 </div>
@@ -135,6 +156,6 @@
                 return false;
             }
         }
-        </script>
+    </script>
     <!-- End of Content Wrapper -->
 @endsection
