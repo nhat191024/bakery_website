1569826c16e62732f646fceec42a9d9433ed8eab
mlemingcapoo <ducvu73@gmail.com>
2024-07-21 17:06:58 +0700

    feat: Add support for English banner content
    Adds English translations for banner titles and content, allowing administrators to create multilingual banner displays. The change ensures that banners can be dynamically displayed in both Vietnamese and English, enhancing the site's accessibility and appeal to a wider audience.

--- app/Http/Controllers/admin/BannerController.php
+++ app/Http/Controllers/admin/BannerController.php
@@ -30,15 +30,19 @@ class BannerController extends Controller
     {
         $request->validate([
             'banner_title' => 'required',
+            'banner_title_en' => 'required',
             'banner_content' => 'required',
+            'banner_content_en' => 'required',
             'banner_image' => 'required',
         ]);
         $bannerTitle = $request->banner_title;
+        $bannerTitleEn = $request->banner_title_en;
         $bannerContent = $request->banner_content;
+        $bannerContentEn = $request->banner_content_en;
         $imageName = time() . '_' . $request->banner_image->getClientOriginalName();
         // Public Folder
-        $request->banner_image->move(public_path('img'), $imageName);
-        $this->bannerService->add($bannerTitle, $bannerContent, $imageName);
+        $request->banner_image->move(public_path('img/home'), $imageName);
+        $this->bannerService->add($bannerTitle, $bannerTitleEn, $bannerContent, $bannerContentEn, $imageName);
         return redirect(route('admin.banner.index'))->with('success', 'Thêm banner thành công');
     }
 
@@ -54,20 +58,24 @@ class BannerController extends Controller
         $request->validate([
             'id' => 'required',
             'banner_title' => 'required',
+            'banner_title_en' => 'required',
             'banner_content' => 'required',
+            'banner_content_en' => 'required',
         ]);
         $id = $request->id;
         $bannerTitle = $request->banner_title;
+        $bannerTitleEn = $request->banner_title_en;
         $bannerContent = $request->banner_content;
+        $bannerContentEn = $request->banner_content_en;
         if ($request->banner_image) {
             $imageName = time() . '_' . $request->banner_image->getClientOriginalName() ?? null;
-            $request->banner_image->move(public_path('img'), $imageName);
+            $request->banner_image->move(public_path('img/home'), $imageName);
             $oldImagePath = $this->bannerService->getById($request->id)->image;
-            if (file_exists(public_path('img') . '/' . $oldImagePath && $oldImagePath != null)) {
-                unlink(public_path('img') . '/' . $oldImagePath);
+            if (file_exists(public_path('img/home') . '/' . $oldImagePath && $oldImagePath != null)) {
+                unlink(public_path('img/home') . '/' . $oldImagePath);
             }
         }
-        $this->bannerService->edit($id, $bannerTitle, $bannerContent, $imageName ?? null);
+        $this->bannerService->edit($id, $bannerTitle, $bannerTitleEn, $bannerContent, $bannerContentEn, $imageName ?? null);
         return redirect(route('admin.banner.index'))->with('success', 'Sửa banner thành công');
     }
 
