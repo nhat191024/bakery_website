b7d72cfabb4fe0a3b1186a82655896f49018cff7
mlemingcapoo <ducvu73@gmail.com>
2024-07-26 17:06:03 +0700

    feat: Add phone number field to messages
    Adds a phone number field to the `Message` model and includes it in the creation process. This allows users to provide their phone number when sending messages.

--- app/Service/client/MessageService.php
+++ app/Service/client/MessageService.php
@@ -4,16 +4,18 @@ namespace App\Service\client;
 
 use App\Models\Message;
 
-class MessageService 
+class MessageService
 {
     public function getAll() {
         return Message::all();
     }
 
     public function create($table) {
+        // dd($table);
         Message::create([
             'name' => $table['name'],
             'email' => $table['email'],
+            'phone' => $table['phone'],
             'subject' => $table['subject'],
             'message' => $table['message'],
         ]);
