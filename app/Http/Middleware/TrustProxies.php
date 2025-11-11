ef7d682182ce58078470e4d519deb612e845f320
Fumino The Fish <richberchannel01@gmail.com>
2025-11-11 14:55:58 +0700

    feat: define trusted proxies in TrustProxies middleware

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,11 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.0.0.0/8',
+        '172.16.0.0/12',
+        '192.168.0.0/16',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
@@ -20,7 +24,7 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers =
-        Request::HEADER_X_FORWARDED_FOR |
+    Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
